b3c307b4b14f08c8fee0fbc160f2d37c3ea88c7a
coderameen <coderameen@gmail.com>
2024-10-29 23:28:04 +0530

    aded

---
+++ sorting/built_in_sorting.py
@@ -0,0 +1,23 @@
+arr = [8,7,6,2,1,9]
+
+#.sort(), it modify existing array
+# arr.sort()
+# print(arr)
+
+#.sort() only works for list/array
+
+#
+arr1 = sorted(arr)# new array crate
+print(arr1)
+
+
+
+
+myarr = [[1,4],[1,3],[9,5],[2,0],[6,4]]
+# myarr.sort()#sort based on 1st index
+# print(myarr)
+
+
+
+myarr.sort(key=lambda x:x[1])
+print(myarr)
