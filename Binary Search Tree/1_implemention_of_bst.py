af8890adea231337ff9a13426df15b52bb3a12f7
coderameen <coderameen@gmail.com>
2024-12-07 00:15:31 +0530

    added code for bst

---
+++ Binary Search Tree/1_implemention_of_bst.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self,key):
+        self.key = key
+        self.left = None
+        self.right = None
+        
+def insert(root,key):
+    if root is None:
+        return Node(key)
+    else:
+        if root.key == key:
+            return root
+        elif root.key < key:
+            root.right = insert(root.right, key)
+        else:
+            root.left = insert(root.left, key)
+    return root
+
+#To dislpay: inorder
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.key, end=" ")
+        inorder(root.right)
+        
+r = Node(50)
+r = insert(r,30)
+r = insert(r,20)
+r = insert(r,40)
+r = insert(r,70)
+r = insert(r,60)
+r = insert(r,80)
+inorder(r)
